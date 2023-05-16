707f41e3a79e02e6e2bfd0e36d462a8ed819a61a
Swetha A S <swetha2001as@gmail.com>
2023-05-17 01:32:49 +0530

    study list

---
+++ basics/list.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  List name = [ 'meez','aish', 'allz'];
+  print(name);
+
+  //add a new list item
+  // add, addAll,insert
+   name.add('ponnu');
+   print(name);
+
+   //remove item from the list
+   //remove,removeAt, removeWhere,removeRange,removeLast
+   name.remove('ponnu');
+   print(name);
+
+
+}
--- basics/variables.dart
+++ basics/variables.dart
@@ -1,5 +1,18 @@
 void main() {
   for( int i=0; i<5; i++){
     print('hii $i');
-  }
+  } // for loop
+
+
+  bool isNight = false;
+  print(isNight); //bool
+
+  dynamic name = 'meez';
+  print(name);
+  String wish = whishes();  
+  
+} 
+
+String whishes(){
+  return 'hapieee';
 }
