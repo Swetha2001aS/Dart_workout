51b15636be833fa14c8758dc38c21ee5200a4f95
Swetha A S <swetha2001as@gmail.com>
2023-05-17 02:07:03 +0530

    study final and const

---
+++ basics/final_const.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+void main() {
+
+  final name = 'meez';
+  final String realname = 'meenus';
+
+  const pi = 3.14;
+  const double gravity = 9.8;
+
+
+}
+
+class Circle {
+  final color = 'red';
+  static const pi = 3.14;
+
+}
