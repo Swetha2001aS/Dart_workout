a30e1a7da2a4b6d50f871cd1cdafcc6d507d6f25
Swetha A S <swetha2001as@gmail.com>
2023-05-17 15:55:22 +0530

    study optional positional parameter

--- functions/demo.dart
+++ functions/demo.dart
@@ -1,5 +1,7 @@
 void main(){
   perimeter();
+  findperimeter(4,5);
+  area(3, 2);
 }
 void perimeter(){
 
@@ -10,3 +12,10 @@ void perimeter(){
   print("the perimeter is $perimeter");
 
 }
+void findperimeter(int len, int bre){
+
+  int perimeter = 2*( len + bre);
+  print("the perimeter is $perimeter");
+ 
+}
+void area(int len,int bre) => print("the area is ${len*bre}");
---
+++ functions/optional_positional_parameters.dart
@@ -0,0 +1,20 @@
+void main() {
+
+printplace("kdlr", "tsr", "ekm");
+print(" ");
+printcontries("usa", "india"); 
+}
+//required parameter
+void printplace(String name1, String name2, String name3){
+  print("name is $name1");
+  print("name is $name2");
+  print("name is $name3");
+  
+}
+//optional parameter
+void printcontries(String name1, String name2, [String? name3]){
+  print("name is $name1");
+  print("name is $name2");
+  print("name is $name3");
+  
+}
