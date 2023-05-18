027a7414862c01d2f7294317d2f850590a2c1fcb
Swetha A S <swetha2001as@gmail.com>
2023-05-18 22:47:32 +0530

    change inheritance

--- oops/inheritance.dart
+++ oops/inheritance.dart
@@ -1,19 +1,15 @@
  
  
  void main(){
-   var dog = Dog();
-   dog.breed = "Labrador";
-   dog.color = "black";
+   var dog = Dog("Labrador","black");
    dog.bark();
    dog.eat();
 
-   var cat = Cat();
-   cat.color = "white";
-   cat.age = 6;
+   var cat = Cat("white");
+      cat.age = 6;
    cat. meow();
 
-   var animal = Animal();
-   animal.color = "yellow";
+   var animal = Animal("yellow");
    animal.eat();
 
 
@@ -22,6 +18,11 @@
  
  class Animal{
    String? color;
+   Animal(String color){
+    this.color = color;
+
+   }
+
    void eat(){
       print("eat");
    } 
@@ -29,13 +30,24 @@
 
  class Dog extends Animal{
    String? breed;
+   Dog(String breed,String color) :super("black"){
+     
+     this.breed = breed;
+     this.color = color;
+
+   }
    void bark(){
        print("bark");
    }
  }
 
   class Cat extends Animal{
+
    int? age;
+   Cat(String age) : super("6"){
+
+    age = age;
+   }
    void meow(){
        print("Meow");
    }
